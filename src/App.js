59b7c58cda425e67922cdc5a9d8c83cb7a89f4ae
Chetan J <janachet@amazon.com>
2023-11-01 13:26:28 +0530

    reset project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,6 @@
-import Navbar from "./components/Navbar";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Link1 from "./pages/Link1";
-import Link2 from "./pages/Link2";
-import Link3 from "./pages/Link3";
-import Link4 from "./pages/Link4";
-import Home from "./pages/Home";
-
 function App() {
   return (
-
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/link1' element={<Link1 />} />
-        <Route path='/link2' element={<Link2 />} />
-        <Route path='/link3' element={<Link3 />} />
-        <Route path='/link4' element={<Link4 />} />
-      </Routes>
-    </BrowserRouter>
+    <h1>Hello world</h1>
   );
 }
 
--- src/pages/Link4.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Link4 = () => {
-  return (
-    <div>Link4</div>
-  )
-}
-
-export default Link4
